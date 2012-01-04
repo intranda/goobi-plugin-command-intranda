b7b492487ab368fb7c357f7fa7862a9d3eb8d416
Robert Sehr <robert.sehr@intranda.com>
2012-01-04 15:32:21 +0000

    added error plugin

---
+++ intrandaCommandPlugins/src/de/intranda/goobi/plugins/ReportProblemCommand.java
@@ -0,0 +1,212 @@
+package de.intranda.goobi.plugins;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+import org.apache.log4j.Logger;
+import org.goobi.production.cli.CommandResponse;
+import org.goobi.production.cli.helper.WikiFieldHelper;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.plugin.interfaces.ICommandPlugin;
+import org.goobi.production.plugin.interfaces.IPlugin;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import de.sub.goobi.Beans.HistoryEvent;
+import de.sub.goobi.Beans.Schritt;
+import de.sub.goobi.Beans.Schritteigenschaft;
+import de.sub.goobi.Persistence.ProzessDAO;
+import de.sub.goobi.Persistence.SchrittDAO;
+import de.sub.goobi.helper.Helper;
+import de.sub.goobi.helper.HelperSchritte;
+import de.sub.goobi.helper.enums.HistoryEventType;
+import de.sub.goobi.helper.enums.PropertyType;
+import de.sub.goobi.helper.enums.StepEditType;
+import de.sub.goobi.helper.enums.StepStatus;
+import de.sub.goobi.helper.exceptions.DAOException;
+
+@PluginImplementation
+public class ReportProblemCommand implements ICommandPlugin, IPlugin {
+
+	private static final Logger logger = Logger.getLogger(ReportProblemCommand.class);
+	private static final String ID = "reportProblem";
+	private static final String NAME = "ReportProblem Command Plugin";
+	private HashMap<String, String> parameterMap;
+
+	@Override
+	public String getId() {
+		return ID;
+	}
+
+	@Override
+	public PluginType getType() {
+		return PluginType.Command;
+	}
+
+	@Override
+	public String getTitle() {
+		return NAME;
+	}
+
+	@Override
+	public String getDescription() {
+		return NAME;
+	}
+
+	@Override
+	public void setParameterMap(HashMap<String, String> parameterMap) {
+		this.parameterMap = parameterMap;
+	}
+
+	@Override
+	public CommandResponse validate() {
+		if (!this.parameterMap.containsKey("stepId")) {
+			String title = "Missing parameter";
+			String message = "No parameter 'stepId' defined.";
+			return new CommandResponse(title, message);
+			// } else if (!this.parameterMap.containsKey("destinationStepId")) {
+			// String title = "Missing parameter";
+			// String message = "No parameter 'destinationStepId' defined.";
+			// return new CommandResponse(title, message);
+		} else if (!this.parameterMap.containsKey("errorMessage")) {
+			String title = "Missing parameter";
+			String message = "No parameter 'errorMessage' defined.";
+			return new CommandResponse(title, message);
+		} else if (!this.parameterMap.containsKey("destinationStepName")) {
+			String title = "Missing parameter";
+			String message = "No parameter 'destinationStepName' defined.";
+			return new CommandResponse(title, message);
+		}
+
+		// } else if (!this.parameterMap.containsKey("user")) {
+		// String title = "Missing parameter";
+		// String message = "No parameter 'user' defined.";
+		// return new CommandResponse(title, message);
+		String stepId = this.parameterMap.get("stepId");
+		try {
+			Integer.parseInt(stepId);
+		} catch (Exception e) {
+			String title = "Wrong value";
+			String message = "value for parameter 'stepId' is not a valid number.";
+			return new CommandResponse(title, message);
+		}
+		// String destinationStepId = this.parameterMap.get("destinationStepId");
+		// try {
+		// Integer.parseInt(destinationStepId);
+		// } catch (Exception e) {
+		// String title = "Wrong value";
+		// String message = "value for parameter 'destinationStepId' is not a valid number.";
+		// return new CommandResponse(title, message);
+		// }
+
+		return null;
+	}
+
+	@Override
+	public CommandResponse execute() {
+		Date myDate = new Date();
+		Integer sourceid = Integer.parseInt(this.parameterMap.get("stepId"));
+		String destinationTitle = this.parameterMap.get("destinationStepName");
+		// Integer targetid = Integer.parseInt(this.parameterMap.get("destinationStepName"));
+		String errorMessage = this.parameterMap.get("errorMessage");
+		SchrittDAO dao = new SchrittDAO();
+		ProzessDAO pdao = new ProzessDAO();
+		try {
+			Schritt source = dao.get(sourceid);
+			source.setBearbeitungsstatusEnum(StepStatus.LOCKED);
+			source.setEditTypeEnum(StepEditType.MANUAL_SINGLE);
+			HelperSchritte.updateEditing(source);
+			source.setBearbeitungsbeginn(null);
+			Schritt temp = null;
+			for (Schritt s : source.getProzess().getSchritteList()) {
+				if (s.getTitel().equals(destinationTitle)) {
+					temp = s;
+				}
+			}
+			if (temp != null) {
+				temp.setBearbeitungsstatusEnum(StepStatus.OPEN);
+				temp.setCorrectionStep();
+				temp.setBearbeitungsende(null);
+				Schritteigenschaft se = new Schritteigenschaft();
+				// Benutzer ben = (Benutzer) Helper.getManagedBeanValue("#{LoginForm.myBenutzer}");
+				se.setTitel(Helper.getTranslation("Korrektur notwendig"));
+				se.setWert("[automatic] " + errorMessage);
+				se.setType(PropertyType.messageError);
+				se.setCreationDate(myDate);
+				se.setSchritt(temp);
+				source.getProzess().setWikifield(WikiFieldHelper.getWikiMessage(source.getProzess().getWikifield(), "error", errorMessage));
+
+				temp.getEigenschaften().add(se);
+				dao.save(temp);
+				source.getProzess()
+						.getHistory()
+						.add(new HistoryEvent(myDate, temp.getReihenfolge().doubleValue(), temp.getTitel(), HistoryEventType.stepError, temp
+								.getProzess()));
+				/*
+				 * alle Schritte zwischen dem aktuellen und dem Korrekturschritt wieder schliessen
+				 */
+				@SuppressWarnings("unchecked")
+				List<Schritt> alleSchritteDazwischen = Helper.getHibernateSession().createCriteria(Schritt.class)
+						.add(Restrictions.le("reihenfolge", source.getReihenfolge())).add(Restrictions.gt("reihenfolge", temp.getReihenfolge()))
+						.addOrder(Order.asc("reihenfolge")).createCriteria("prozess").add(Restrictions.idEq(source.getProzess().getId())).list();
+				for (Iterator<Schritt> iter = alleSchritteDazwischen.iterator(); iter.hasNext();) {
+					Schritt step = iter.next();
+					step.setBearbeitungsstatusEnum(StepStatus.LOCKED);
+					// if (step.getPrioritaet().intValue() == 0)
+					step.setCorrectionStep();
+					step.setBearbeitungsende(null);
+					Schritteigenschaft seg = new Schritteigenschaft();
+					seg.setTitel(Helper.getTranslation("Korrektur notwendig"));
+					seg.setWert(Helper.getTranslation("KorrekturFuer") + temp.getTitel() + ": " + errorMessage);
+					seg.setSchritt(step);
+					seg.setType(PropertyType.messageImportant);
+					seg.setCreationDate(new Date());
+					step.getEigenschaften().add(seg);
+				}
+				pdao.save(source.getProzess());
+			}
+
+		} catch (DAOException e) {
+			logger.error(e);
+			String title = "Error during execution";
+			String message = "An error occured: " + e.getMessage();
+			return new CommandResponse(title, message);
+		}
+
+		String title = "Command executed";
+		String message = "Problem reported";
+		return new CommandResponse(title, message);
+	}
+
+	@Override
+	public CommandResponse help() {
+		String title = "Command help";
+		String message = "this is the help for a command";
+		return new CommandResponse(title, message);
+	}
+
+	@Override
+	public boolean usesHttpSession() {
+		return false;
+	}
+
+	@Override
+	public void setHttpResponse(HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHttpRequest(HttpServletRequest resp) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
