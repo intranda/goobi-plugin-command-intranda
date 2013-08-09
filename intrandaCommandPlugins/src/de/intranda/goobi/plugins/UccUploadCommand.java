75bb06d8e7b8fd252ade2161409ccdc8cacf76ae
Robert Sehr <robert.sehr@intranda.com>
2013-08-09 15:01:28 +0200

    added trace messages

--- intrandaCommandPlugins/src/de/intranda/goobi/plugins/UccUploadCommand.java
+++ intrandaCommandPlugins/src/de/intranda/goobi/plugins/UccUploadCommand.java
@@ -27,126 +27,134 @@ import de.sub.goobi.config.ConfigMain;
 @PluginImplementation
 public class UccUploadCommand implements ICommandPlugin, IPlugin {
 
-	private static final Logger logger = Logger.getLogger(UccUploadCommand.class);
+    private static final Logger logger = Logger.getLogger(UccUploadCommand.class);
 
-	private static final String ID = "ucc_upload";
-	// private static final String NAME = "UCC Upload Command Plugin";
-	// private static final String VERSION = "1.0.20111109";
+    private static final String ID = "ucc_upload";
+    // private static final String NAME = "UCC Upload Command Plugin";
+    // private static final String VERSION = "1.0.20111109";
 
-	private HashMap<String, String> parameterMap;
-//	private HttpServletResponse response;
-	private HttpServletRequest request;
+    private HashMap<String, String> parameterMap;
+    //	private HttpServletResponse response;
+    private HttpServletRequest request;
 
-	@Override
-	public PluginType getType() {
-		return PluginType.Command;
-	}
+    @Override
+    public PluginType getType() {
+        return PluginType.Command;
+    }
 
-	@Override
-	public String getTitle() {
-		return ID;
-	}
+    @Override
+    public String getTitle() {
+        return ID;
+    }
 
-	
-	public String getId() {
-		return ID;
-	}
-	
-	@Override
-	public String getDescription() {
-		return ID;
-	}
+    public String getId() {
+        return ID;
+    }
 
-	@Override
-	public void setParameterMap(HashMap<String, String> parameterMap) {
-		this.parameterMap = parameterMap;
-	}
+    @Override
+    public String getDescription() {
+        return ID;
+    }
 
-	@Override
-	public boolean usesHttpSession() {
-		return true;
-	}
+    @Override
+    public void setParameterMap(HashMap<String, String> parameterMap) {
+        this.parameterMap = parameterMap;
+    }
 
-	@Override
-	public void setHttpResponse(HttpServletResponse resp) {
-//		response = resp;
-	}
+    @Override
+    public boolean usesHttpSession() {
+        return true;
+    }
 
-	@Override
-	public void setHttpRequest(HttpServletRequest req) {
-		request = req;
-	}
+    @Override
+    public void setHttpResponse(HttpServletResponse resp) {
+        //		response = resp;
+    }
 
-	@Override
-	public CommandResponse validate() {
+    @Override
+    public void setHttpRequest(HttpServletRequest req) {
+        request = req;
+    }
 
-		if (!parameterMap.containsKey("processId")) {
-			String title = "Missing parameter";
-			String message = "No parameter 'processId' defined";
-			return new CommandResponse(400, title, message);
-			// return new CommandResponse(title, message);
-		}
+    @Override
+    public CommandResponse validate() {
 
-		return null;
-	}
+        if (!parameterMap.containsKey("processId")) {
+            String title = "Missing parameter";
+            String message = "No parameter 'processId' defined";
+            return new CommandResponse(400, title, message);
+            // return new CommandResponse(title, message);
+        }
 
-	@Override
-	public CommandResponse execute() {
+        return null;
+    }
 
-		Integer processId = Integer.parseInt(parameterMap.get("processId"));
-		Session session = HibernateUtilOld.getSessionFactory().openSession();
-		File archive = new File(ConfigMain.getParameter("tempfolder"), processId + ".zip");
-		OutputStream out = null;
-		try {
-			out = new FileOutputStream(archive);
-			InputStream in = request.getInputStream();
-			int numRead;
-			byte[] buf = new byte[4096];
-			while ((numRead = in.read(buf)) >= 0) {
-				out.write(buf, 0, numRead);
-			}
-			out.flush();
+    @Override
+    public CommandResponse execute() {
+        logger.trace("Execute UccUpload");
+        Integer processId = Integer.parseInt(parameterMap.get("processId"));
+        logger.trace("process id is " + processId);
+        Session session = HibernateUtilOld.getSessionFactory().openSession();
+        logger.trace("get hibernate session");
+        File archive = new File(ConfigMain.getParameter("tempfolder"), processId + ".zip");
+        logger.trace("created temporary file " + archive.getAbsolutePath());
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(archive);
+            logger.trace("write to temporay file");
+            InputStream in = request.getInputStream();
+            logger.trace("read request input stream");
+            int numRead;
+            byte[] buf = new byte[4096];
+            while ((numRead = in.read(buf)) >= 0) {
+                out.write(buf, 0, numRead);
+            }
+            out.flush();
+            logger.trace("finished data import");
+            Prozess process = (Prozess) session.get(Prozess.class, processId);
+            logger.trace("loaded process " + process.getTitel());
+            File metaDest = new File(process.getMetadataFilePath());
+            logger.trace("metadata file is " + metaDest.getAbsolutePath());
+            File anchorDest = new File(process.getMetadataFilePath().replace("meta.xml", "meta_anchor.xml"));
 
-			Prozess process = (Prozess) session.get(Prozess.class, processId);
-			File metaDest = new File(process.getMetadataFilePath());
-			File anchorDest = new File(process.getMetadataFilePath().replace("meta.xml", "meta_anchor.xml"));
+            File metaSource = new File(archive, "meta.xml");
+            File anchorSource = new File(archive, "meta_anchor.xml");
+            metaSource.copyTo(metaDest);
+            logger.trace("metadata file is overwritten");
+            if (anchorSource.exists()) {
+                logger.trace("anchor file exist");
+                anchorSource.copyTo(anchorDest);
+                logger.trace("anchor file is overwritten");
+            }
+        } catch (Exception e) {
+            logger.error(e);
+            String title = "Error during execution";
+            String message = "An error occured: " + e.getMessage();
+            return new CommandResponse(500, title, message);
+            //			return new CommandResponse(title, message);
+        } finally {
+            session.close();
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    logger.error(e);
+                }
+            }
+        }
 
-			File metaSource = new File(archive, "meta.xml");
-			File anchorSource = new File(archive, "meta_anchor.xml");
+        String title = "Command executed";
+        String message = "Message to process log added";
+        return new CommandResponse(200, title, message);
+        //		return new CommandResponse(title, message);
+    }
 
-			metaSource.copyTo(metaDest);
-			if (anchorSource.exists()) {
-				anchorSource.copyTo(anchorDest);
-			}
-		} catch (Exception e) {
-			logger.error(e);
-			String title = "Error during execution";
-			String message = "An error occured: " + e.getMessage();
-			 return new CommandResponse(500, title, message);
-//			return new CommandResponse(title, message);
-		} finally {
-			session.close();
-			if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					logger.error(e);
-				}
-			}
-		}
-
-		String title = "Command executed";
-		String message = "Message to process log added";
-		 return new CommandResponse(200, title, message);
-//		return new CommandResponse(title, message);
-	}
-
-	@Override
-	public CommandResponse help() {
-		String title = "Command ucc_upload";
-		String message = "this command accepts a ucc sip file for a specific process.";
-		message += "\n The parameter 'processId' defines to which process the sip file belongs. The sip file is sended via http post request.";
-		// return new CommandResponse(200, title, message);
-		return new CommandResponse(title, message);
-	}
+    @Override
+    public CommandResponse help() {
+        String title = "Command ucc_upload";
+        String message = "this command accepts a ucc sip file for a specific process.";
+        message += "\n The parameter 'processId' defines to which process the sip file belongs. The sip file is sended via http post request.";
+        // return new CommandResponse(200, title, message);
+        return new CommandResponse(title, message);
+    }
 }
